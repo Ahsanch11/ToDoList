55716247d45f21aba63375394754e68f247a7d38
root <root@ICB13.localdomain>
2018-01-21 17:31:08 -0800

    upto sass updatedd

---
+++ src/js/todo-correct.js
@@ -0,0 +1,33 @@
+
+import addListItem from './todo-correct_2.js';
+import 'bootstrap';
+import '../css/styles.scss';
+
+$(document).ready(function(){
+
+    
+  //var addListItem = require('./todo-correct_2.js');
+  
+  console.log(`..loading`);
+
+
+  var listItems =[];
+
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    var item_value = $('#list_item_input').val();
+    var item_obj = {item_value: item_value, completed: false}
+    $('#list_item_input').val('');
+    addListItem(item_obj,listItems);
+
+  });
+
+
+/////////////////////////////////////////////
+
+
+///////////////////////////////////////////   ///////////////////////
+
+
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,99 @@
+var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports =
 {
 
-entry: './todo-correct',
+entry: './src/js/todo-correct',
 output: {
 
 	path: __dirname,
-	filename:'bundle-new-1.js'
+	filename:'dist/js/bundle-es5.js'
 
 
 },
-	watch:true
+	watch:true,
+	
+
+
+
+	module:{
+
+		rules:[
+
+		{
+			test:/\.js$/,
+			exclude:/node_modules/,
+			use:{
+			
+				loader:'babel-loader',
+				options:{
+					presets:[ 'env' ,'stage-3']
+			
+				} //  wwe can write but npm auto load all in env['babel-preset-es2015','es2016','es2017']   //we can just write es2015
+			
+			}//
+		},
+		{
+			test:/\.scss$/,
+			use:   ExtractTextPlugin.extract({
+
+				use: [
+
+				{
+					loader:'css-loader',
+
+					options:{
+
+						url:false
+
+					}
+				},
+
+					'sass-loader'
+					]
+
+
+
+
+			})
+
+
+
+
+
+		}
+
+					
+
+	]
+
+	
+	}
+	,
+	
+	plugins: [
+
+
+		new webpack.ProvidePlugin({
+
+			'$':'jquery',
+			'jQuery':'jquery'
+
+
+
+		}),
+
+		new ExtractTextPlugin({
+
+				filename: ./dist/css/styles.css'
+
+		})
+
+
+	]
+
+
+
 
 }
